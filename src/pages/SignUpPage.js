913216b94db4ac8af3ca02278a0ce368b9f3739a
Mehdi Ali <yarpamehdi2000@gmail.com>
2024-09-17 17:59:32 +0500

    feat: added project boiler with and routing

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Signup from '../features/auth/components/Signup'
+
+function SignUpPage() {
+  return (
+    <Signup></Signup>
+  )
+}
+
+export default SignUpPage
