e6ee45aa2d86a698b03bdcd351c30e0ff40034f3
Mehdi Ali <yarpamehdi2000@gmail.com>
2024-09-22 18:30:28 +0500

    added checkout page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
+import ProductDetailPage from './pages/ProductDetailPage';
 
 
 const router = createBrowserRouter([
@@ -28,6 +30,14 @@ const router = createBrowserRouter([
   {
     path: "/cart",
     element: <CartPage></CartPage>,
+  },
+  { 
+    path: '/checkout',
+    element: <Checkout></Checkout>,
+  },
+  { 
+    path: '/product-detail',
+    element: <ProductDetailPage></ProductDetailPage>,
   },
 ]);
 
--- src/features/product-list/components/ProductList.js
+++ src/features/product-list/components/ProductList.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, incrementAsync, selectCount } from "./productListSlice";
+import { increment, incrementAsync, selectCount } from "../productSlice";
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
 import {
   Dialog,
@@ -22,6 +22,7 @@ import {
   PlusIcon,
   Squares2X2Icon,
 } from "@heroicons/react/20/solid";
+import { Link } from "react-router-dom";
 
 const sortOptions = [
   { name: "Most Popular", href: "#", current: true },
@@ -315,6 +316,7 @@ export default function ProductList() {
 
                     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
                       {products.map((product) => (
+                        <Link to="/product-detail">
                         <div key={product.id} className="group relative">
                           <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                             <img
@@ -343,6 +345,7 @@ export default function ProductList() {
                             </p>
                           </div>
                         </div>
+                        </Link>
                       ))}
                     </div>
                   </div>
